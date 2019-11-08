76a5fe9bdf4c80b96399f4a63313ad34c04506a0
Sean Hutchison <sean.hutchison@ruralsourcing.com>
2019-11-08 14:09:12 -0700

    day 3 setup

--- src/main/java/com/hutchison/calendar/days/day3/Day3.java
+++ src/main/java/com/hutchison/calendar/days/day3/Day3.java
@@ -2,11 +2,7 @@ package com.hutchison.calendar.days.day3;
 
 import com.hutchison.calendar.Day;
 
-import java.util.List;
-
 public class Day3 extends Day {
-
-   private final List<String> input = super.getInput();
 
     @Override
     public void part1() {
