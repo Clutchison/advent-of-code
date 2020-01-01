d60f718512148aaf4921a9c1278f265dbf4b5d8b
Sean <xskhutchx@gmail.com>
2020-01-01 16:54:55 -0700

    Day 8 Setup

---
+++ src/main/java/com/hutchison/calendar/days/y2019/day8/Day8.java
@@ -0,0 +1,15 @@
+package com.hutchison.calendar.days.y2019.day8;
+
+import com.hutchison.calendar.Day;
+
+public class Day8 extends Day {
+    @Override
+    public void part1() {
+
+    }
+
+    @Override
+    public void part2() {
+
+    }
+}
